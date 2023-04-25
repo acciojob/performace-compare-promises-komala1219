a5675f1fb0cc85b058cbab0f2fd85b1ce743e232
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-25 15:28:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ const apiUrls = [
 ];
 
 // You can write your code here
+
+const fetchApi = async (url) => {
+  const startTime = new Date();
+  await fetch(url); // replace this with your actual API call
+  const endTime = new Date();
+  return (endTime - startTime) / 1000; // return time taken in seconds
+};
+
+const fetchData = async () => {
+  const allStart = new Date();
+  const allResults = await Promise.all(apiUrls.map(fetchApi));
+  const allEnd = new Date();
+  const allTimeTaken = (allEnd - allStart) / 1000;
+
+  const anyStart = new Date();
+  const anyResult = await Promise.any(apiUrls.map(fetchApi));
+  const anyEnd = new Date();
+  const anyTimeTaken = (anyEnd - anyStart) / 1000;
+
+  document.getElementById('output-all').innerHTML = allTimeTaken;
+  document.getElementById('output-any').innerHTML = anyTimeTaken;
+};
